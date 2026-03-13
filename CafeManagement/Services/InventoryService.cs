90b3382ad8a829dd94cbd33a74da40c31b109af9
Giang0402 <giangb2205976@student.ctu.edu.vn>
2026-03-13 19:17:50 +0700

    feat(pos): Hoàn thành module POS - PIN, KDS, CustomerDisplay, SignalR

--- CafeManagement/Services/InventoryService.cs
+++ CafeManagement/Services/InventoryService.cs
@@ -13,7 +13,8 @@ public class InventoryService
     /// </summary>
     public Task DeductStockAsync(int orderId)
     {
-        // TODO: TV3 implement
-        throw new NotImplementedException("TV3: InventoryService.DeductStockAsync");
+        // Tạm thời Console.WriteLine để test thay vì throw lỗi. TV3 sẽ code tiếp ở đây.
+        Console.WriteLine($"[InventoryService] Đã trừ nguyên liệu cho đơn hàng #{orderId}");
+        return Task.CompletedTask;
     }
 }
